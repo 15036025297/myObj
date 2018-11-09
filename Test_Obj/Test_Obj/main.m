ca1d8b7ff9f50bf14afa19c43cb3c2e476ae85a9
15036025297 <15036025297@163.com>
2018-11-09 14:52:52 +0800

    Add files via upload

---
+++ Test_Obj/Test_Obj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test_Obj
+//
+//  Created by chenhui on 2018/11/9.
+//  Copyright © 2018年 chenhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Test_Obj/Test_ObjTests/Test_ObjTests.m
@@ -0,0 +1,37 @@
+//
+//  Test_ObjTests.m
+//  Test_ObjTests
+//
+//  Created by chenhui on 2018/11/9.
+//  Copyright © 2018年 chenhui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_ObjTests : XCTestCase
+
+@end
+
+@implementation Test_ObjTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
